e4505f4bf1d11158c07c7eeb2228b216c11c5014
Chen.Zhidong <njutczd@gmail.com>
2015-11-26 15:25:58 +0800

    允许直接执行send request

--- Client.php
+++ Client.php
@@ -49,7 +49,7 @@ class Client {
         return isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.122 Safari/537.36";
     }
 
-    private static function sendRequest($request) {
+    public static function sendRequest($request) {
         $t1 = microtime(true);
         $ch = curl_init();
         $options = array(
